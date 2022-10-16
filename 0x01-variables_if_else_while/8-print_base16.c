c7c16c2519b4baa6cb103a1eed8d0eff19a820c8
Nonnyfinest <Nonnyfinest90@gmail.com>
2022-10-16 15:38:35 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 	int num;
+	char af;
 
-	for (num = 0; num <= 16; num++)
-		putchar(num);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (af = 'a'; af <= 'f'; af++)
+		putchar(af);
 
 	putchar('\n');
 
