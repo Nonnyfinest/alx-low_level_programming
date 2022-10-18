cf80a383e32eb2c0c80a60ad275aeb5e55ea5e60
Nonnyfinest <Nonnyfinest90@gmail.com>
2022-10-18 16:35:57 -0700

    alpha_10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alpha 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alpha;
+
+	while (count++ <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
