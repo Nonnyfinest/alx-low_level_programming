182177f4fb7bcbc923c705a8962d8d7495266551
Nonnyfinest <Nonnyfinest90@gmail.com>
2022-10-20 09:49:36 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * author: Nonso
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int C = 1;
+
+	if (C == 1)
+		_putchar('1');
+	else
+		_putchar('0');
+	return (0);
+}
