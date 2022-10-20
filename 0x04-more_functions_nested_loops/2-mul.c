1e68d1d236b111731112174dc2ac1a6dc53ecbbc
Nonnyfinest <Nonnyfinest90@gmail.com>
2022-10-20 11:29:35 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * multiply two int
+ * @a: first num
+ * @b: second num
+ * Return: sum of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
