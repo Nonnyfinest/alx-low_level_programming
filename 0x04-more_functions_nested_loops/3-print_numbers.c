d0314a98512ba6977bbeb97657dd6187e6533cd0
Nonnyfinest <Nonnyfinest90@gmail.com>
2022-10-20 11:51:18 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry
+ * prints from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+		_putchar('a');
+	_putchar('\n');
+}
